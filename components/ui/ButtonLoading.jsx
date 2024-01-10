e4a988c0371d48ea199195ecee69f73dd36c7f61
devdaim6 <daimdev6@gmail.com>
2024-01-10 12:08:36 +0530

    some ui components added and Carousel for Hero added to Components

---
+++ components/ui/ButtonLoading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ButtonLoading = () => {
+  return (
+    <>
+      <span className="loading loading-dots loading-xs"></span>
+    </>
+  );
+};
+
+export default ButtonLoading;
