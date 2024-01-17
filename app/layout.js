406ea6345e7d7085df276ad28f4226b2b7a4a174
devdaim6 <daimdev6@gmail.com>
2024-01-17 18:43:22 +0530

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import Nav from "@/components/Navbar/Nav";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import TopContent from "@/components/Header/TopContent";
 import Footer from "@/components/Footer/Footer";
 import {
@@ -69,6 +71,8 @@ export default function RootLayout({ children }) {
                 <Toaster closeButton position="top-center" />
                 <Nav />
                 {children}
+                <SpeedInsights /> 
+                <Analytics />
                 <Footer />
               </StoreProvider>
             </AuthProvider>
